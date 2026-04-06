cb1c6b6abac610494fde8236eda5c95252eb6167
arburolaromy <57246891+arburolaromy@users.noreply.github.com>
2026-04-06 08:29:02 -0600

    Web Oraca

---
+++ script.js
@@ -0,0 +1,10 @@
+// Activar desplazamiento suave para los enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
